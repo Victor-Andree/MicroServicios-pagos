7b3ec1ee26bb28bf1e9f59a102bd2ced69d7c584
Victor <u21221311@utp.edu.pe>
2025-10-30 13:10:45 -0500

    Orquestacion con ApiGateway, ConfigServer, AdminSpringBoot y correccion de errores menores

--- paymentchainparent/infraestructuradomain/springBootAdmin/src/main/java/com/paymentchain/SpringBootAdminApplication.java
+++ paymentchainparent/infraestructuradomain/springBootAdmin/src/main/java/com/paymentchain/SpringBootAdminApplication.java
@@ -3,10 +3,12 @@ package com.paymentchain;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableAutoConfiguration
@@ -16,6 +18,20 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SpringBootAdminApplication
 {
     public static void main(String[] args) {
+
         SpringApplication.run(SpringBootAdminApplication.class, args);
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .anyRequest().permitAll()
+                )
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+
 }
