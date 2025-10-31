a38fbd0297491ba2053a965023c095cd233bbab8
Victor <u21221311@utp.edu.pe>
2025-10-31 13:11:45 -0500

    Cambios en dependecias, se actualizo las versione en dockerfile respecto a Java 17 a 21, composicion mejorada

--- paymentchainparent/dbfiles/init-user-db.sh
+++ paymentchainparent/dbfiles/init-user-db.sh
@@ -2,8 +2,14 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER springboot WITH PASSWORD 'qwerty';
+
     CREATE DATABASE customer;
-    GRANT ALL PRIVILEGES ON DATABASE customer TO "$POSTGRES_USER";
+    GRANT ALL PRIVILEGES ON DATABASE customer TO springboot;
+
+    CREATE DATABASE product;
+    GRANT ALL PRIVILEGES ON DATABASE product TO springboot;
+
+    CREATE DATABASE transaction;
+    GRANT ALL PRIVILEGES ON DATABASE transaction TO springboot;
 EOSQL
-
-
