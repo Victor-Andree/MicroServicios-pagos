ee721a44788fe0b1df57c4e485dfadb0976e3cd3
VICTOR ANDREE SEVILLANO VEGA <U21221311@utp.edu.pe>
2025-11-02 13:09:16 -0500

    Creacion del Servicio KeyCloak, se agrego los modulos para los token y excepciones

---
+++ paymentchainparent/infraestructuradomain/keycloackAdapter/src/test/java/com/paymentchain/infraestructuradomain/keycloackAppTest.java
@@ -0,0 +1,14 @@
+package com.paymentchain.infraestructuradomain;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+//@SpringBootTest
+//public class keycloackAppTest{
+//    @Test
+//	void contextLoads() {
+//	}
+
+//}
